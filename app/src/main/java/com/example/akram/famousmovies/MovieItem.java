d7bb788bf50e5bc6496f5d69e817d8ece4228b4d
Akram Dweikat <akram.dweikat@hotmail.com>
2017-02-07 13:27:42 +0000

    adding attributes for Movie item

--- app/src/main/java/com/example/akram/famousmovies/MovieItem.java
+++ app/src/main/java/com/example/akram/famousmovies/MovieItem.java
@@ -1,8 +1,38 @@
 package com.example.akram.famousmovies;
 
+import android.media.Image;
+
 /**
  * Created by akram on 06/02/2017.
  */
 
 public class MovieItem {
+
+    private String MovieTitle;
+    private String MovieImage;
+
+    public MovieItem(){
+
+    }
+    public MovieItem(String MovieTitle, String MovieImage){
+        this.MovieTitle = MovieTitle;
+        this.MovieImage  =  MovieImage;
+    }
+
+    public void setMovieTitle(String movieTitle){
+        this.MovieTitle = movieTitle;
+    }
+
+    public void setMovieImage(String movieImageUrl){
+        String baseURL = "http://image.tmdb.org/t/p/w185/";
+        this.MovieImage = baseURL + movieImageUrl ;
+    }
+
+    public String getMovieTitle(){
+        return this.MovieTitle;
+    }
+
+    public String getMovieImage(){
+        return this.MovieImage;
+    }
 }
