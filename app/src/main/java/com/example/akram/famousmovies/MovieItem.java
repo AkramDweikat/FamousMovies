d61aa241ed36732f0cd681a5a818b8851d46591f
Akram Dweikat <akram.dweikat@hotmail.com>
2017-02-07 13:40:56 +0000

    adding movie attributes  to activity

--- app/src/main/java/com/example/akram/famousmovies/MovieItem.java
+++ app/src/main/java/com/example/akram/famousmovies/MovieItem.java
@@ -1,38 +1,65 @@
 package com.example.akram.famousmovies;
 
-import android.media.Image;
-
 /**
  * Created by akram on 06/02/2017.
  */
 
 public class MovieItem {
 
-    private String MovieTitle;
-    private String MovieImage;
+    private String movieTitle;
+    private String movieImage;
+    private String movieDate;
+    private Integer movieAvgVoteRate;
+    private String moviePlotSynopsis;
 
     public MovieItem(){
 
     }
     public MovieItem(String MovieTitle, String MovieImage){
-        this.MovieTitle = MovieTitle;
-        this.MovieImage  =  MovieImage;
+        this.movieTitle = MovieTitle;
+        this.movieImage =  MovieImage;
     }
 
     public void setMovieTitle(String movieTitle){
-        this.MovieTitle = movieTitle;
+        this.movieTitle = movieTitle;
+    }
+
+    public String getMovieTitle(){
+        return this.movieTitle;
     }
 
     public void setMovieImage(String movieImageUrl){
         String baseURL = "http://image.tmdb.org/t/p/w185/";
-        this.MovieImage = baseURL + movieImageUrl ;
+        this.movieImage = baseURL + movieImageUrl ;
     }
 
-    public String getMovieTitle(){
-        return this.MovieTitle;
-    }
 
     public String getMovieImage(){
-        return this.MovieImage;
+        return this.movieImage;
     }
+    public void setMovieDate(String date){
+        this.movieDate = date;
+    }
+
+    public String getMovieDate(){
+        return this.movieDate;
+    }
+
+    public void setMovieAvgVoteRate(Integer rate){
+        this.movieAvgVoteRate = rate;
+    }
+
+    public Integer getMovieAvgVoteRate(){
+        return this.movieAvgVoteRate;
+    }
+
+    public void setMoviePlotSynopsis(String plot){
+        this.moviePlotSynopsis = plot;
+    }
+
+    public String getMoviePlotSynopsis(){
+        return this.moviePlotSynopsis;
+    }
+
+
 }
