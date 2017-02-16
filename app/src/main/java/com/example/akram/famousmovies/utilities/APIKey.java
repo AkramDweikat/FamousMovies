69862bef0d333f21e60dabd3a342a58a46a7907d
Akram Dweikat <akram.dweikat@hotmail.com>
2017-02-16 15:26:51 +0000

    adding APIKEY File

---
+++ app/src/main/java/com/example/akram/famousmovies/utilities/APIKey.java
@@ -0,0 +1,18 @@
+package com.example.akram.famousmovies.utilities;
+
+/**
+ * Created by akram on 07/02/2017.
+ */
+
+public final class APIKey {
+
+    private final String APIKEY ="";
+
+    APIKey(){
+
+    }
+
+    public String getAPIKEY() {
+        return APIKEY;
+    }
+}
